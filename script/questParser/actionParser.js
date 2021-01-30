56a40bc506fd36da653908f7afb499d00b903109
Vsevolod Drobyshevskiy <lleelleeoo@yandex-team.ru>
2021-01-31 02:00:19 +0300

    states for actionParser.js + tests

--- script/questParser/actionParser.js
+++ script/questParser/actionParser.js
@@ -1,4 +1,37 @@
-actionParser = () => {}
+const allowedStates = ['health', 'mind', 'sleep', 'bullets'];
+
+actionParser = (actionString) => {
+    const actions = actionString.split('@');
+    const link = actions.shift();
+    const nextEvent = parseInt(link);
+    const add = {};
+    const remove = {};
+
+    if (link !== `${nextEvent}`) {
+        throw new Error(`link is missing in ${actionString}`);
+    }
+
+    actions.forEach((action) => {
+        const [state, sign, valueString] = action.split(/(\+|-)/);
+        const value = parseInt(valueString);
+
+        if (!allowedStates.includes(state)) {
+            throw new Error(`invalid state '${state}' in '${actionString}'`)
+        }
+
+        if (sign === '+') {
+            add[state] ? add[state] += value : add[state] = value;
+        } else if (sign === '-') {
+            remove[state] ? remove[state] -= value : remove[state] = -value;
+        } else {
+            throw new Error(`invalid action '${action}' in '${actionString}'`);
+        }
+    });
+
+    return {
+        nextEvent, add, remove
+    }
+}
 
 // for tests
 try {
--- script/questParser/actionParser.spec.js
+++ script/questParser/actionParser.spec.js
@@ -1,1 +1,39 @@
-const {} = require('./eventParser.js');
+const { actionParser } = require('./actionParser.js');
+
+// 2@health+1@mind+1@sleep+3@bullets-1@item+someItem@item-otherItem
+describe('actionParser', () => {
+    describe('🐙 actionParser helper', () => {
+        it('should throw error, if first is not a link', () => {
+            expect(() => actionParser('health+1')).toThrow();
+            expect(() => actionParser('42@health+1')).not.toThrow();
+        });
+
+        it('should return propper struct', () => {
+            expect(actionParser('11')).toEqual({nextEvent: 11, add: {}, remove: {}});
+        });
+
+        it('should parse link to next', () => {
+            expect(actionParser('86')).toHaveProperty('nextEvent', 86);
+        });
+
+        it('should parse link to next, if actions passed', () => {
+            expect(actionParser('23@health+1')).toHaveProperty('nextEvent', 23);
+        });
+
+        it('should throw error, if action is invalid', () => {
+            expect(() => actionParser('2@health/2')).toThrow();
+        });
+
+        it('should parse add actions', () => {
+            expect(actionParser('23@health+20')).toHaveProperty('add', { health: 20 })
+        });
+
+        it('should parse remove action', () => {
+            expect(actionParser('23@health-25')).toHaveProperty('remove', { health: -25 })
+        });
+
+        it('should throw error, if state is unknown', () => {
+            expect(() => actionParser('23@pew+146')).toThrow();
+        });
+    });
+});
