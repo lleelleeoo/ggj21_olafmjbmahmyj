a62b30e6cc2dc0409d337af656dc36756bf91842
Vsevolod Drobyshevskiy <lleelleeoo@yandex-team.ru>
2021-01-31 02:37:47 +0300

    fix remove calculation

--- script/questParser/actionParser.spec.js
+++ script/questParser/actionParser.spec.js
@@ -47,6 +47,7 @@ describe('actionParser', () => {
             ${ '12@sleep+10@sleep+23' }  | ${ 12 }      | ${{ sleep: 33 }}| ${{}}
             ${ '12@sleep+10@sleep-23' }  | ${ 12 }      | ${{ sleep: 10 }}| ${{ sleep: 23 }}
             ${ '42@mind+10@bullets-1' }  | ${ 42 }      | ${{ mind: 10 }}| ${{ bullets: 1 }}
+            ${ '42@bullets-1@bullets-2' }  | ${ 42 }      | ${{}}| ${{ bullets: 3 }}
         `('🐙 actionParser get $actions, returns $nextEvent, $add and $remove', ({ actions, nextEvent, add, remove}) => {
             expect(actionParser(actions)).toEqual({nextEvent, add, remove})
         })
