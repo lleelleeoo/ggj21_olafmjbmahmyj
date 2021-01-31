83ef961709d8e5ff30b1cb2078605d34b69a7f95
Vsevolod Drobyshevskiy <lleelleeoo@yandex-team.ru>
2021-01-31 10:57:26 +0300

    feat: recalculate state on action

--- script/engine/gameController.js
+++ script/engine/gameController.js
@@ -10,15 +10,37 @@ let eventId;
 let event;
 
 const init = () => {
-    eventId = 0;
+    updateCurrentEvent(0 || 1);
     state = {...initState};
-    event = parseEvent(Quest[eventId]);
 }
 
 getCurrentState = () => state;
 getEventCaption = () => event.caption;
 getOptions = () => event.options.map(({caption, action}) => ({caption, action}));
 getPicture = () => event.picture;
+
+const processAction = (actionString) => {
+    const { nextEvent, add, remove } = actionParser(actionString);
+    const nextState = { ...state };
+
+    for (item in add) {
+        if (item === 'item') continue;
+        nextState[item] = state[item] + add[item];
+    }
+
+    for (item in remove) {
+        if (item === 'item') continue
+        nextState[item] = state[item] - remove[item];
+    }
+
+    updateCurrentEvent(nextEvent);
+    updateCurrentState(nextState);
+};
+
+const updateCurrentEvent = (nextId) => {
+    eventId = nextId - 1;
+    event = parseEvent(Quest[eventId]);
+}
 
 const updateCurrentState = ({ health, mind, sleep, bullets }) => {
     if (health !== undefined) state.health = health;
