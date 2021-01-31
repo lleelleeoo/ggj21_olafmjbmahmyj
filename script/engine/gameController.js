99b8bba1431a9a4d1d13c5952394e7e97e82ccde
Vsevolod Drobyshevskiy <lleelleeoo@yandex-team.ru>
2021-01-31 04:15:44 +0300

    feat: add state logic

--- script/engine/gameController.js
+++ script/engine/gameController.js
@@ -5,13 +5,24 @@ const initStrate = {
     bullets: 6,
 };
 
-const state = {...initStrate};
+let state;
+
+const init = () => {
+    state = {...initStrate};
+}
 
 getCurrentState = () => state;
 
-updateCurrentState = ({ health, mind, sleep, bullets }) => {
+const updateCurrentState = ({ health, mind, sleep, bullets }) => {
     if (health !== undefined) state.health = health;
     if (mind !== undefined) state.mind = mind;
     if (sleep !== undefined) state.sleep = sleep;
     if (bullets !== undefined) state.bullets = bullets;
 }
+
+// for tests
+try {
+    module.exports = {
+        getCurrentState, updateCurrentState, init
+    }
+} catch {}
--- script/engine/gameController.spec.js
+++ script/engine/gameController.spec.js
@@ -1,3 +1,46 @@
-describe('gameController', () => {
+const { getCurrentState, updateCurrentState, init } = require('./gameController.js');
 
+describe('gameController', () => {
+    beforeEach(() => {
+        init();
+    });
+
+    it('should content init state', () => {
+        expect(getCurrentState()).toEqual({
+            health: 100,
+            mind: 100,
+            sleep: 100,
+            bullets: 6,
+        })
+    });
+
+    it('should update state', () => {
+        updateCurrentState({
+            health: 30,
+            mind: 10,
+            sleep: 17,
+            bullets: 1,
+        });
+
+        expect(getCurrentState()).toEqual({
+            health: 30,
+            mind: 10,
+            sleep: 17,
+            bullets: 1,
+        });
+    });
+
+    it('should update part of state', () => {
+        updateCurrentState({
+            mind: 10,
+            bullets: 1,
+        });
+
+        expect(getCurrentState()).toEqual({
+            health: 100,
+            mind: 10,
+            sleep: 100,
+            bullets: 1,
+        });
+    });
 });
